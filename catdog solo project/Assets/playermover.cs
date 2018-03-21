ed04511fad91729df3e2d35232b5b67e011a0121
SSZ4 <sjinzzang21@naver.com>
2018-03-21 20:21:56 +0900

    fasdf
    asdfasd

---
+++ catdog solo project/Assets/playermover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermover : MonoBehaviour {
+	public float speed = 10.0f;
+	public float rotSpeed = 90.0f;
+	Rigidbody rigid;
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horize = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(horize, 0.0f, vertical);
+		rigid.AddForce(movement);
+	}
+}
